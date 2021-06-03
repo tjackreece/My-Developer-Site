70a1c9cc84c1fffe1bb298e387a79a86d9627fb6
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-06-03 13:59:34 -0400

    wip

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname, "client/build")));
+
+server.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 console.log(process.env.USER); // env USER=gabriel
 console.log(process.env.SHELL); // env SHELL=/bin/zhs
