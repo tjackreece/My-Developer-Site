cb62bddb148364f9336a071b596bbf06ef14b3aa
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-06-03 14:04:54 -0400

    wip

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -8,11 +7,7 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use(express.static(path.join(__dirname, "client/build")));
-
-server.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+server.use(express.static(path.join(__dirname, "client/build"))); // static assets
 
 console.log(process.env.USER); // env USER=gabriel
 console.log(process.env.SHELL); // env SHELL=/bin/zhs
@@ -25,12 +20,19 @@ const PORT = process.env.PORT || 5000;
 
 console.log("port is -> ", PORT);
 
+server.get("/", (req, res) => {
+	// sending back index.html
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 server.get("/api", (req, res) => {
 	res.json({ message: `${process.env.COHORT} ROCKS` });
 });
+
 server.use((req, res) => {
-	res.status(404).json({ messge: "Great its working" });
+	res.status(404).json({ message: "not found sorry" });
 });
+
 server.listen(PORT, () => {
 	console.log(`listening on ${PORT}`);
 });
